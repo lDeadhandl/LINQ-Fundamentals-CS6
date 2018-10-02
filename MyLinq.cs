fbff9fc1f4d58818dc8429ffbf0718ee1ea719ac
Unknown <stefan.vladetic7@gmail.com>
2018-10-02 14:04:06 -0400

    LINQ Intro
    Used Extension Methods and Lambda Expressions

---
+++ MyLinq.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace LinqSamples
+//{
+//    //when making an extension method must use public static 
+//    public static class MyLinq
+//    {
+//        // by adding this to the parameter we no have created an
+//        // extension method to all IEnumerables that do not have
+//        // to be called by an instance
+//        public static int Count<T>(this IEnumerable<T> sequence)
+//        {
+//            int count = 0;
+//            foreach(var item in sequence)
+//            {
+//                count += 1;
+//            }
+//            return count;
+//        }
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using LinqSamples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //using func type used more 
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) => x + y;
+
+
+            //using action type used less than func types
+            // always returns void
+            Action<int> write = x => Console.WriteLine(x);
+            write(square(add(3,5)));
+            //creating an array of Employee using IEnumerable
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott"},
+                new Employee { Id = 2, Name = "Chris"}
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex"}
+            };
+
+            Console.WriteLine(developers.Count());
+            // Using only method of IEnumerable,GetEnumerator, that allows us to iterate
+            // through and collection of things
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                Console.Write(enumerator.Current.Name);
+            }
+
+            // Complicated way of filtering without lambda
+            foreach(var employee in developers.Where(
+                delegate (Employee employee)
+            {
+                return employee.Name.StartsWith("S");
+            }))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            //instead use lambda expression to simplify above expression
+            foreach (var employee in developers.Where(
+                            e => e.Name.StartsWith("S")))
+               { 
+                   Console.WriteLine(employee.Name);
+               }
+        }
+
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
+        }
+    }
+}
